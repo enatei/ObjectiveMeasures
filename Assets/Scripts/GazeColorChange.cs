5751d74ba99ff3962efec2e4798ab1790e3b6dba
vr_lab <hugo.brument@tuwien.ac.at>
2025-08-01 13:45:40 +0200

    re-working colliders on cylinder to see if it can change color when gazed-at

---
+++ Assets/Scripts/GazeColorChange.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class GazeColorChanger : MonoBehaviour
+{
+    public Color gazeColor = Color.yellow;
+
+    private Color originalColor;
+    private MeshRenderer meshRenderer;
+
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer != null)
+            originalColor = meshRenderer.material.color;
+    }
+
+    // Wird beim Gaze Hover aufgerufen
+    public void OnGazeEnter()
+    {
+        if (meshRenderer != null)
+            meshRenderer.material.color = gazeColor;
+    }
+
+    // Wird beim Gaze Hover verlassen aufgerufen
+    public void OnGazeExit()
+    {
+        if (meshRenderer != null)
+            meshRenderer.material.color = originalColor;
+    }
+}
+
